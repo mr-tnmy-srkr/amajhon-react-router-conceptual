b4ae700a1feb8ee1f903a15d155f98cce4857095
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-20 22:58:25 +0530

    spinner added

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -5,53 +5,51 @@ const Header = () => {
     <div>
       <div>
         <section className="flex flex-col md:flex-row gap-5 md:gap-0 justify-between items-center px-10 py-5 shadow-xl">
-         <Link to="/">
-          <div>
-            <h1 className="text-4xl font-bold">Amajhon</h1>
-          </div>
+          <Link to="/">
+            <div>
+              <h1 className="text-4xl font-bold">Amajhon</h1>
+            </div>
           </Link>
           <nav>
             <ul className="flex gap-5 font-semibold text-xl">
-              
-                <NavLink
-                  to="/"
-                  className={({ isActive, isPending }) =>
-                    isPending
-                      ? "pending"
-                      : isActive
-                      ? "bg-green-200 text-2xl rounded-lg"
-                      : ""
-                  }
-                >
-                  Home
-                </NavLink>
-             
-                <NavLink
-                  to="/products"
-                  className={({ isActive, isPending }) =>
-                    isPending
-                      ? "pending"
-                      : isActive
-                      ? "bg-green-200 text-2xl rounded-lg"
-                      : ""
-                  }
-                >
-                  Products
-                </NavLink>
-              
-                <NavLink
-                  to="/dashboard"
-                  className={({ isActive, isPending }) =>
-                    isPending
-                      ? "pending"
-                      : isActive
-                      ? "bg-green-200 text-2xl rounded-lg"
-                      : ""
-                  }
-                >
-                  Dashboard
-                </NavLink>
-            
+              <NavLink
+                to="/"
+                style={({ isActive, isPending }) => {
+                  return {
+                    fontWeight: isActive ? "bold" : "",
+                    color: isPending ? "red" : "black",
+                    textDecoration: isActive ? "underline" : "",
+                  };
+                }}
+              >
+                Home
+              </NavLink>
+
+              <NavLink
+                to="/products"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "bg-green-200 text-2xl rounded-lg"
+                    : ""
+                }
+              >
+                Products
+              </NavLink>
+
+              <NavLink
+                to="/dashboard"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "bg-green-200 text-2xl rounded-lg"
+                    : ""
+                }
+              >
+                Dashboard
+              </NavLink>
             </ul>
           </nav>
         </section>
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,21 +1,24 @@
 import Footer from "./Footer";
 import Header from "./Header";
+
+import ClockLoader from "react-spinners/ClockLoader";
 import { Outlet, useNavigation } from "react-router-dom";
 
-
-
 const MainLayout = () => {
-
   const navigation = useNavigation();
-console.log(navigation.state === "loading");
-
+  console.log(navigation.state === "loading");
 
   return (
     <div>
-     <Header></Header>
-      <div className="min-h-[80vh] my-10">
-        <Outlet></Outlet>
-      </div>
+      <Header></Header>
+      {navigation.state === "loading" ? (
+        <p className="flex justify-center items-center my-20"><ClockLoader color="#36d7b7" /></p>
+      ) : (
+        <div className="min-h-[80vh] my-10">
+          <Outlet></Outlet>
+        </div>
+      )}
+
       <Footer></Footer>
     </div>
   );
