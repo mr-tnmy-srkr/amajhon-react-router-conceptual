998e5841dd17901b166252fe4adbec4e13548774
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-20 16:23:57 +0530

    dynamic card added

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
-import Products from "../pages/Products/products";
+import Products from "../pages/Products/Products";
 import MainLayout from "../layouts/MainLayout";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Product from "../pages/Product/Product";
 
 const myCreatedRoute = createBrowserRouter([
   // This is a fixed item
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-
     // This is changeable
     children: [
       {
@@ -18,10 +19,17 @@ const myCreatedRoute = createBrowserRouter([
       {
         path: "/products",
         element: <Products></Products>,
+        loader: () => fetch(`https://dummyjson.com/products`),
+      },
+      {
+        path:"/product/:id",
+        element:<Product></Product>,
+        // loader: (params)=>console.log(params.params.id)
+        // loader: ({params})=>fetch(`https://dummyjson.com/products/${params.id}`)
       },
       {
         path: "/dashboard",
-        element: <Products></Products>,
+        element: <Dashboard></Dashboard>,
       },
     ],
   },
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,24 @@
+// import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import ProductsCard from "./ProductsCard";
 
 const Products = () => {
+  const { products } = useLoaderData();
+  // console.log(products);
+
+// or
+
+  /* const [products,setProducts] = useState([]);
+  useEffect(()=>{
+fetch(`https://dummyjson.com/products`)
+.then(res=>res.json())
+.then(data=>setProducts(data.products))
+  },[])
+console.log(products); */
+
   return (
-    <div>
-      <div>Products page</div>
+    <div className="grid grid-cols-3 gap-5 my-10 place-items-center p-5">
+      {products && products?.map((product) => <ProductsCard key={product.id} product={product}></ProductsCard>)}
     </div>
   );
 };
